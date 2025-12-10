675fecd99ae9d098d2bb9c9c7365495b16523540
Linas <linasmg58@gmail.com>
2025-12-10 13:26:29 +0100

    fixed segmentation fault

--- decoy_checker/decoyChecker.c
+++ decoy_checker/decoyChecker.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
 #define GOOD_INDEX 3
 #define dir "/var/tmp/check_decoy.txt"
 
 
 int get_num_from_file() {
     FILE *fptr = fopen(dir, "r");
-    int num;
+    if (fptr == NULL) {
+        perror("Error opening file");
+        fclose(fptr);
+        exit(EXIT_FAILURE);
+    }
 
-    fscanf(fptr,"%d", &num);
+    int num;
+    if (fscanf(fptr,"%d", &num) != 1) {
+        perror("Error reading from file");
+        fclose(fptr);
+        exit(EXIT_FAILURE);
+    }
     fclose(fptr);
     return num;
 }
@@ -21,10 +31,9 @@ int main() {
     time_t t1, t2;
     int last_num;
 
+    last_num = get_num_from_file();
     if (stat(dir, &attr) == 0)
         t1 = attr.st_mtim.tv_sec;
-
-    last_num = get_num_from_file();
 
     while (1) {
         if (stat(dir, &attr) == 0)
